23454b21c0303ac7ab3e590cb2a8bebe3c4f9d1a
Samuel Aiedo <samueldefreitasaiedo@gmail.com>
2024-07-19 10:55:53 -0300

    amigo-secreto update

--- js/app.js
+++ js/app.js
@@ -17,11 +17,12 @@ function sortear() {
     let sorteio = document.getElementById("lista-sorteio");
 
     for (let i = 0; i < amigos.length; i++) {
-        if (i == amigos.length) {
-        sorteio.innerHTML = sorteio.innerHTML + amigos[i] + " --> " + amigos[0] + '<br>'
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + " --> " + amigos[0] + '<br>'
         } else {
              sorteio.innerHTML = sorteio.innerHTML + amigos[i] + " --> " + amigos[i + 1] + '<br>'
         }
+
     }
 }
 
